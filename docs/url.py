cb8b3326559a07f6ab2286a8f8d2c8ec78756eb6
root <root@mail.czlab.ca>
2022-10-03 02:18:39 +0000

    updated az url

---
+++ docs/url.py
@@ -0,0 +1,9 @@
+
+# HD Canada
+hdca = "https://"+hdcaurl+"/api/productsvc/v1/products/1001221502/store/7259?fields=BASIC_SPA&lang=en"
+
+# AZ China
+azcn = "https://"+azcnurl+"/api/marketplaces/AAHKV2X7AFYLW/products/B087ZCBWFH?merchantId=B087ZCBWFH"
+
+# AZ Com
+azcom = "https://"+azcomrul+"/shop/products/B087ZCBWFH"
